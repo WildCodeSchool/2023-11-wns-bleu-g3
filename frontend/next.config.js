6365f502a8eb94fe04c821d1946167e3632f843f
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-04-24 14:33:00 +0200

    windows11 fast refresh fix

---
+++ frontend/next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+webpack: (config, context) => {
+  // Enable polling based on env variable being set
+  if(process.env.NEXT_WEBPACK_USEPOLLING) {
+    config.watchOptions = {
+      poll: 500,
+      aggregateTimeout: 300
+    }
+  }
+  return config
+},
+}
+
+module.exports = nextConfig
