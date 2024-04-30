f78fc48bb1e14ee91657613f174fb750a57662c8
096benjaminbenoit <096benjaminbenoit@gmail.com>
2024-04-30 16:00:49 +0200

    homepage hero banner ok

--- frontend/src/components/header-visitor.tsx
+++ frontend/src/components/header-visitor.tsx
@@ -1,26 +1,49 @@
 import { useEffect, useState } from "react";
 import ModalAuthentication from "./modal-authentication";
 import useScroll from "@/hooks/useScroll";
+import useScreenSize from "@/hooks/useScreenSize";
 
 export default function HeaderVisitor() {
   const scrollY = useScroll();
+  const screenSize = useScreenSize();
 
-  return (
-    <header
-      className={`flex justify-between p-4 sticky ${
-        scrollY > 90
-          ? "bg-shore shadow-sm transition duration-300 ease-in-out"
-          : "transition duration-300 ease-in-out"
-      } top-0`}
-    >
-      <div className="flex justify-center items-center gap-2">
-        <img src="../../img/greenfoot-logo.png" alt="" className="w-14" />
+  if (screenSize.width > 1025) {
+    return (
+      <header
+        className={`flex justify-between p-4 z-40 w-full fixed top-0 ${
+          scrollY > 90
+            ? "bg-shore shadow-sm transition duration-300 ease-in-out"
+            : "transition duration-300 ease-in-out"
+        } top-0`}
+      >
+        <div className="flex justify-center items-center gap-2">
+          <img src="../../img/greenfoot-logo.png" alt="" className="w-14" />
 
-        <h1 className="text-reef text-xl md:text-3xl font-bold drop-shadow-lg">
-          GreenFoot
-        </h1>
-      </div>
-      <ModalAuthentication />
-    </header>
-  );
+          <h1 className="text-reef text-xl md:text-3xl font-bold drop-shadow-lg">
+            GreenFoot
+          </h1>
+        </div>
+        <ModalAuthentication />
+      </header>
+    );
+  } else {
+    return (
+      <header
+        className={`flex justify-between p-4 fixed w-full z-40 ${
+          scrollY > 90
+            ? "bg-shore shadow-sm transition duration-150 ease-in-out"
+            : "transition duration-150 ease-in-out"
+        } top-0`}
+      >
+        <div className="flex justify-center items-center gap-2">
+          <img src="../../img/greenfoot-logo.png" alt="" className="w-14" />
+
+          <h1 className="text-reef text-xl md:text-3xl font-bold drop-shadow-lg">
+            GreenFoot
+          </h1>
+        </div>
+        <ModalAuthentication />
+      </header>
+    );
+  }
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,23 +1,23 @@
-import Footer from "@/components/footer";
-import { useProfileQuery } from "@/graphql/generated/schema";
 import LayoutVisitor from "@/layouts/layout-visitor";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const { data: user } = useProfileQuery({
-    errorPolicy: "ignore",
-  });
   return (
-    // <main className="">
-
-    // </main>
     <LayoutVisitor>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="text-reef">
-        </h1>
-      </div>
+      <section className="relative bg-[url(https://images.unsplash.com/photo-1428908728789-d2de25dbd4e2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center bg-no-repeat">
+        <div className="px-4 py-48 sm:px-6 flex flex-col lg:justify-center h-screen items-center lg:px-8 lg:pt-10">
+          <h2 className="text-2xl font-medium text-reef sm:text-4xl text-center">
+            Réduisez votre empreinte carbone, préservez notre avenir.
+          </h2>
+          <div className="mt-8 flex flex-col gap-4 text-center">
+            <a
+              href="#"
+              className="btn bg-lightPearl text-reef px-6 md:px-24 md:text-lg shadow-sm hover:bg-reef hover:text-lightPearl transition ease-in-out"
+            >
+              Calculez votre empreinte carbone
+            </a>
+          </div>
+        </div>
+      </section>
     </LayoutVisitor>
   );
 }
