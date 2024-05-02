36b508297c0a931c12e619cf68fc0d8ef0f56d52
096benjaminbenoit <096benjaminbenoit@gmail.com>
2024-05-02 11:01:43 +0200

    add react component icons

---
+++ frontend/src/components/icon.tsx
@@ -0,0 +1,11 @@
+export default function Icon({
+  name,
+  size,
+  color,
+}: {
+  name: string;
+  size: string;
+  color: string;
+}) {
+  return <span className={`material-icons text-${color} text-${size}`}>{name}</span>;
+}
