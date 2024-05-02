b3396b4d0b78892fb2abdac8344a536788bd890e
Anaïs <127841238+AnaisCav@users.noreply.github.com>
2024-05-02 10:50:57 +0200

    ajout google icons (#17)

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="fr" className="bg-pearl">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
