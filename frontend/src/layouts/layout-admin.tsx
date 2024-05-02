82f4a8d22accc40cb64a22349f242156fc6da87f
096benjaminbenoit <096benjaminbenoit@gmail.com>
2024-05-02 16:28:23 +0200

    admin layout ok

---
+++ frontend/src/layouts/layout-admin.tsx
@@ -0,0 +1,16 @@
+import SideAdmin from "@/components/side-admin";
+import TopbarAdmin from "@/components/topbar-admin";
+import React, { use, useState } from "react";
+
+export default function LayoutAdmin({ children }: { children: any }) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <TopbarAdmin isOpen={isOpen} setIsOpen={setIsOpen} />
+      <main className="flex justify-between h-full">
+        <SideAdmin isOpen={isOpen} />
+        <div className="p-4 md:p-6 w-full absolute md:relative">{children}</div>
+      </main>
+    </>
+  );
+}
---
+++ frontend/src/pages/admin/dashboard.tsx
@@ -0,0 +1,9 @@
+import LayoutAdmin from "@/layouts/layout-admin";
+
+export default function Dashboard() {
+  return (
+    <LayoutAdmin>
+      <h2>Je suis le dashboard</h2>
+    </LayoutAdmin>
+  );
+}
