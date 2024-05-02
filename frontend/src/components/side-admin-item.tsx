5197dcb609a38188b7371c51ece2a80d43324313
Benjamin Benoit <119290452+096benjaminbenoit@users.noreply.github.com>
2024-05-02 16:32:42 +0200

    admin layout ok (#21)

---
+++ frontend/src/components/side-admin-item.tsx
@@ -0,0 +1,18 @@
+export default function SideAdminItem({
+  name,
+  href,
+}: {
+  name: string;
+  href: string;
+}) {
+  return (
+    <li>
+      <a
+        href={href}
+        className="flex items-center text-sm p-2 text-white rounded-lg  bg-reef hover:bg-white hover:text-black btn transition ease-in-out"
+      >
+        {name}{" "}
+      </a>
+    </li>
+  );
+}
