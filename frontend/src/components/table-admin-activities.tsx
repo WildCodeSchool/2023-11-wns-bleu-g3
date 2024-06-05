069c413511856da1566f0899ab5811f3bb124ed6
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-06-05 03:39:44 +0200

    table pagination + style done

--- frontend/src/components/table-admin-activities.tsx
+++ frontend/src/components/table-admin-activities.tsx
@@ -1,16 +1,44 @@
 import Icon from "@/components/icon";
+import { useGetActivitiesTypesPaginationQuery } from "@/graphql/generated/schema";
+import { useQuery } from "@apollo/client";
+import { useEffect, useState } from "react";
+
+const PAGE_SIZE = 8;
 
 export default function TableActivities() {
+  const [page, setPage] = useState(0);
+  const [notEndPage, setNotEndPage] = useState(true);
 
+  const { data, loading, error } = useGetActivitiesTypesPaginationQuery({
+    variables: {
+      limit: PAGE_SIZE,
+      offset: page * PAGE_SIZE,
+    },
+  });
 
-    
+  //this useEffect regards if next page doesnt contain thhe expected number of items from page size
+  useEffect(() => {
+    if (data) {
+      setNotEndPage(data.getActivitiesTypesPagination.length === PAGE_SIZE);
+    }
+  }, [data]);
 
+  if (loading) {
+    return <p>Chargement...</p>;
+  }
 
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
 
+  const activities = data?.getActivitiesTypesPagination || [];
 
   return (
-    <div className="relative  overflow-x-auto shadow-md m-auto my-12 sm:rounded-lg w-4/5">
-      <table className="w-full text-sm text-left  rtl:text-right text-gray-500 ">
+    <div className="relative  overflow-x-auto shadow-md m-auto my-6 sm:rounded-lg w-4/5">
+      <table
+        className="w-full text-sm text-left  rtl:text-right text-gray-500 "
+        id="toptable"
+      >
         <thead className="text-xs  uppercase bg-shore  text-anchor">
           <tr>
             <th scope="col" className="px-6 py-3">
@@ -30,76 +58,69 @@ export default function TableActivities() {
           </tr>
         </thead>
         <tbody>
-          <tr className=" border-t border-gray-500 bg-pearl text-reef  hover:bg-shore hover:text-anchor">
-            <th
-              scope="row"
-              className="px-6 py-4 font-medium  whitespace-nowrap "
-            >
-              Chemise
-            </th>
+          {activities.map((activities) => (
+            <tr className=" border-t border-gray-500 bg-pearl text-reef  hover:bg-shore hover:text-anchor">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium  whitespace-nowrap "
+              >
+                {activities.name}
+              </th>
 
-            <td className="px-6 py-4">Vetements</td>
-            <td className="px-6 py-4">267</td>
-            <td className="px-6 py-4">co2/pc</td>
-            <td className=" py-4">
-              <a href="#" className="font-medium text-anchor hover:underline ">
-                <Icon name="edit" size="" color="reef" />
-              </a>
-            </td>
-            <td className=" py-4">
-              <a href="#" className="font-medium text-error hover:underline">
-                <Icon name="delete" size="" color="" />
-              </a>
-            </td>
-          </tr>
+              <td className="px-6 py-4">{activities.category}</td>
+              <td className="px-6 py-4">{activities.emissions}</td>
+              <td className="px-6 py-4">{activities.unit}</td>
+              <td className=" py-4">
+                <a
+                  href="#"
+                  className="font-medium text-anchor hover:underline "
+                >
+                  <Icon name="edit" size="" color="reef" />
+                </a>
+              </td>
+              <td className=" py-4">
+                <a href="#" className="font-medium text-error hover:underline">
+                  <Icon name="delete" size="" color="" />
+                </a>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
+
       <nav
-        className="flex bg-pearl text-reef items-center flex-column flex-wrap md:flex-row justify-between pt-4  border-t border-gray-500"
+        className="flex bg-pearl text-reef items-center flex-column flex-wrap md:flex-row justify-between pt-4  border-t border-gray-500 "
         aria-label="Table navigation"
       >
-        <ul className="inline-flex m-auto mb-4  -space-x-px rtl:space-x-reverse text-sm h-8">
+        <ul className="inline-flex m-auto mb-4  -space-x-px rtl:space-x-reverse text-sm h-8 hover:bg-lightPearl">
           <li>
-            <a
-              href="#"
-              className="flex items-center justify-center px-3 h-8 ms-0 leading-tight  bg-pearl text-reef border border-anchor rounded-s-lg  font-medium  hover:bg-reef hover:text-white"
+            <button
+              className="flex items-center justify-center px-3 h-8 ms-0 leading-tight  bg-pearl text-reef border border-anchor rounded-s-lg  font-medium  hover:bg-lightPearl hover:text-anchor"
+              disabled={!page}
+              onClick={() => setPage((prev) => prev - 1)}
             >
-              Previous
-            </a>
+              &lt; Précédente
+            </button>
           </li>
           <li>
             <a
               href="#"
-              className="flex items-center justify-center px-3 h-8  leading-tight  bg-pearl text-reef border border-anchor   font-medium  hover:bg-reef hover:text-white"
+              className="flex items-center justify-center px-7 h-8  leading-tight  bg-pearl text-reef border border-anchor   font-medium   "
             >
-              1
+              Page {page + 1}
             </a>
           </li>
           <li>
-            <a
-              href="#"
-              className="flex items-center justify-center px-3 h-8  leading-tight  bg-pearl text-reef border border-anchor   font-medium  hover:bg-reef hover:text-white"
+            <button
+              className="rounded-e-lg  flex items-center justify-center px-6 h-8  leading-tight  bg-pearl text-reef border border-anchor   font-medium  hover:bg-lightPearl hover:text-anchor"
+              onClick={() => {
+                if (notEndPage) {
+                  setPage((prev) => prev + 1);
+                }
+              }}
             >
-              2
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              aria-current="page"
-              className="flex items-center justify-center px-3 h-8  leading-tight  bg-lightPearl text-anchor border border-anchor   font-medium  hover:bg-shore hover:text-white"
-            >
-              3
-            </a>
-          </li>
-
-          <li>
-            <a
-              href="#"
-              className="rounded-e-lg  flex items-center justify-center px-3 h-8  leading-tight  bg-pearl text-reef border border-anchor   font-medium  hover:bg-reef hover:text-white"
-            >
-              Next
-            </a>
+              Suivante &gt;
+            </button>
           </li>
         </ul>
       </nav>
--- frontend/src/pages/admin/activities.tsx
+++ frontend/src/pages/admin/activities.tsx
@@ -7,7 +7,7 @@ export default function Activities() {
     <LayoutAdmin>
     
       <div className="m-auto w-4/5">
-        <h2 className="text-2xl  font-semibold mt-8">Liste Type Activités</h2>
+        <h2 className="text-2xl  font-semibold mt-5">Liste Type Activités</h2>
         <br />
         <p>
           Ici, vous avez le choix de visualiser les types d'activités que
