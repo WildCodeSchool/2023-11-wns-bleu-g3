bdf10cac78d6c2a35d3ffa75abd6707e82a8b9b8
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-06-07 01:43:07 +0200

    table styling

--- frontend/src/components/table-admin-activities.tsx
+++ frontend/src/components/table-admin-activities.tsx
@@ -35,29 +35,66 @@ export default function TableActivities() {
 
   return (
     <div className="relative overflow-x-auto shadow-md m-auto my-6 sm:rounded-lg w-4/5">
-      <table className="w-full text-xs md:text-sm text-left rtl:text-right text-gray-500" id="toptable">
+      <table
+        className="w-full text-xs md:text-sm text-left rtl:text-right text-gray-500"
+        id="toptable"
+      >
         <thead className="text-xs uppercase bg-shore text-anchor">
           <tr>
-            <th scope="col" className="px-2 py-1 md:px-6 md:py-3">Nom</th>
-            <th scope="col" className="px-2 py-1 md:px-6 md:py-3 hidden md:table-cell">Catégorie</th>
-            <th scope="col" className="px-2 py-1 md:px-6 md:py-3 hidden md:table-cell">Emissions</th>
-            <th scope="col" className="px-2 py-1 md:px-6 md:py-3 hidden md:table-cell">Unité</th>
-            <th scope="col" className="px-1 py-1 md:px-2 md:py-3">Actions</th>
+            <th scope="col" className="px-2 py-1 md:px-6 md:py-3">
+              Nom
+            </th>
+            <th
+              scope="col"
+              className="px-2 py-1 md:px-6 md:py-3 hidden md:table-cell"
+            >
+              Catégorie
+            </th>
+            <th
+              scope="col"
+              className="px-2 py-1 md:px-6 md:py-3 hidden md:table-cell"
+            >
+              Emissions
+            </th>
+            <th
+              scope="col"
+              className="px-2 py-1 md:px-6 md:py-3 hidden md:table-cell"
+            >
+              Unité
+            </th>
+            <th scope="col" className="px-1 py-1 md:px-2 md:py-3">
+              Actions
+            </th>
           </tr>
         </thead>
         <tbody>
           {activities.map((activity) => (
-            <tr key={activity.id} className="border-t border-gray-500 bg-pearl text-reef hover:bg-shore hover:text-anchor">
-              <th scope="row" className="px-2 py-1 md:px-6 md:py-4 font-medium whitespace-nowrap">
+            <tr
+              key={activity.id}
+              className="border-t border-gray-500 bg-pearl text-reef hover:bg-shore hover:text-anchor"
+            >
+              <th
+                scope="row"
+                className="px-2 py-1 md:px-6 md:py-4 font-medium whitespace-nowrap"
+              >
                 {activity.name}
               </th>
-              <td className="px-2 py-1 md:px-6 md:py-4 hidden md:table-cell">{activity.category}</td>
-              <td className="px-2 py-1 md:px-6 md:py-4 hidden md:table-cell">{activity.emissions}</td>
-              <td className="px-2 py-1 md:px-6 md:py-4 hidden md:table-cell">{activity.unit}</td>
+              <td className="px-2 py-1 md:px-6 md:py-4 hidden md:table-cell">
+                {activity.category}
+              </td>
+              <td className="px-2 py-1 md:px-6 md:py-4 hidden md:table-cell">
+                {activity.emissions}
+              </td>
+              <td className="px-2 py-1 md:px-6 md:py-4 hidden md:table-cell">
+                {activity.unit}
+              </td>
               <td className="px-1 py-1 md:px-2 md:py-4 flex space-x-1 md:space-x-2">
-              <Link href={`/admin/upd_activity/${activity.id}`} className="font-medium text-anchor hover:underline">
+                <Link
+                  href={`/admin/upd_activity/${activity.id}`}
+                  className="font-medium text-anchor hover:underline"
+                >
                   <Icon name="edit" size="" color="reef" />
-               </Link>
+                </Link>
                 <a href="#" className="font-medium text-error hover:underline">
                   <Icon name="delete" size="" color="" />
                 </a>
@@ -67,11 +104,18 @@ export default function TableActivities() {
         </tbody>
       </table>
 
-      <nav className="flex bg-pearl text-reef items-center flex-col md:flex-row justify-between pt-2 md:pt-4 border-t border-gray-500" aria-label="Table navigation">
+      <nav
+        className="flex bg-pearl text-reef items-center flex-col md:flex-row justify-between pt-2 md:pt-4 border-t border-gray-500"
+        aria-label="Table navigation"
+      >
         <ul className="inline-flex m-auto mb-2 md:mb-4 -space-x-px rtl:space-x-reverse text-xs md:text-sm h-6 md:h-8">
           <li>
             <button
-              className={`flex items-center justify-center px-2 md:px-3 h-6 md:h-8 ms-0 leading-tight bg-pearl border border-anchor rounded-s-lg font-medium ${!page ? 'text-gray-400' : 'text-reef hover:bg-lightPearl hover:text-anchor'}`}
+              className={`flex items-center justify-center px-2 md:px-3 h-6 md:h-8 ms-0 leading-tight bg-pearl border border-anchor rounded-s-lg font-medium ${
+                !page
+                  ? "text-gray-400"
+                  : "text-reef hover:bg-lightPearl hover:text-anchor"
+              }`}
               disabled={!page}
               onClick={() => setPage((prev) => prev - 1)}
             >
