e840387368cea2fe4fa6d0b3a900040cc1e6377b
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-06-07 03:01:11 +0200

    form upd activ style 1/2

--- frontend/src/pages/admin/upd_activity/[id].tsx
+++ frontend/src/pages/admin/upd_activity/[id].tsx
@@ -14,15 +14,86 @@ export default function ProductDetails() {
     skip: typeof id === "undefined",
   });
 
-  const product = data?.getActivityTypesById;
+  const activity = data?.getActivityTypesById;
 
   return (
     <LayoutAdmin>
-      {typeof product === "undefined" ? (
-        "Produit Plus Disponible..."
-      ) : (
-        <div></div>
-      )}
+      <form className="max-w-3xl mx-auto mt-5 p-5">
+        <div>
+          <p className="pb-2 text-lg font-medium text-gray-700">
+            Modifie type activité
+          </p>
+          <h1 className="text-3xl font-bold underline text-reef">
+            {activity?.name || ""}
+          </h1>
+          <br />
+          <p className="text-sm text-gray-600 mb-1">
+            Vous avez la possibilité de modifier les caractéristiques de cette
+            type d'activité, comme la catégorie, le valeur de émission de CO
+            <sub>2</sub> et l'unité pour la mesure.
+          </p>
+          <br />
+        </div>
+        <div className="mb-3">
+          <label
+            htmlFor="firstName"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Catégorie
+          </label>
+          <select
+            id="category"
+            name="category"
+            className="bg-gray-50 shadow-sm border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+            defaultValue={activity?.category || ""}
+          >
+            <option selected>{activity?.category || ""}</option>
+            <option value="US">United States</option>
+            <option value="CA">Canada</option>
+            <option value="FR">France</option>
+            <option value="DE">Germany</option>
+          </select>
+        </div>
+        <div className="mb-3">
+          <label
+            htmlFor="lastName"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Emissions CO<sub>2</sub>
+          </label>
+          <input
+            type="text"
+            name="lastName"
+            id="lastName"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3"
+            defaultValue={activity?.emissions || ""}
+          />
+        </div>
+        <div className="mb-3">
+          <label
+            htmlFor="nickname"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Unité Mesure
+          </label>
+          <input
+            type="text"
+            name="nickname"
+            id="nickname"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3"
+            defaultValue={activity?.unit || ""}
+            required
+          />
+        </div>
+        
+        
+        <button
+          type="submit"
+          className="text-lightPearl bg-reef hover:bg-shore hover:text-anchor focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 mt-2 py-2.5 text-center"
+        >
+          Modifier
+        </button>
+      </form>
     </LayoutAdmin>
   );
 }
