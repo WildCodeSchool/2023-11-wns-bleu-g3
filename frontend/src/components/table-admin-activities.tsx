20bd2fbc48814bac7169e595ac17bb4b439908f1
Ricardo Martinho <117286481+ricardomrcruz@users.noreply.github.com>
2024-07-01 16:44:06 +0200

    ActivTypes Table + Update Form (#44)
    * enum formula change
    
    * gql query add
    
    * table styling
    
    * table upd route add
    
    * route upd add
    
    * form upd activ style 1/2
    
    * form
    
    * table update style
    
    * add enums form upd activ
    
    * update form
    
    * fix schema
    
    * after merge / fix select option output

--- frontend/src/components/table-admin-activities.tsx
+++ frontend/src/components/table-admin-activities.tsx
@@ -2,6 +2,7 @@ import Icon from "@/components/icon";
 import { useGetActivitiesTypesPaginationQuery } from "@/graphql/generated/schema";
 import { useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 const PAGE_SIZE = 8;
 
@@ -16,7 +17,6 @@ export default function TableActivities() {
     },
   });
 
-  //this useEffect regards if next page doesnt contain thhe expected number of items from page size
   useEffect(() => {
     if (data) {
       setNotEndPage(data.getActivitiesTypesPagination.length === PAGE_SIZE);
@@ -34,51 +34,30 @@ export default function TableActivities() {
   const activities = data?.getActivitiesTypesPagination || [];
 
   return (
-    <div className="relative  overflow-x-auto shadow-md m-auto my-6 sm:rounded-lg w-4/5">
-      <table
-        className="w-full text-sm text-left  rtl:text-right text-gray-500 "
-        id="toptable"
-      >
-        <thead className="text-xs  uppercase bg-shore  text-anchor">
+    <div className="relative overflow-x-auto shadow-md m-auto my-6 sm:rounded-lg w-4/5">
+      <table className="w-full text-xs md:text-sm text-left rtl:text-right text-gray-500" id="toptable">
+        <thead className="text-xs uppercase bg-shore text-anchor">
           <tr>
-            <th scope="col" className="px-6 py-3">
-              Nom
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Catégorie
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Emissions
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Unité
-            </th>
-            <th scope="col" className="px-2 py-3"></th>
-            <th scope="col" className="px-2 py-3"></th>
+            <th scope="col" className="px-2 py-1 md:px-6 md:py-3">Nom</th>
+            <th scope="col" className="px-2 py-1 md:px-6 md:py-3 hidden md:table-cell">Catégorie</th>
+            <th scope="col" className="px-2 py-1 md:px-6 md:py-3 hidden md:table-cell">Emissions</th>
+            <th scope="col" className="px-2 py-1 md:px-6 md:py-3 hidden md:table-cell">Unité</th>
+            <th scope="col" className="px-1 py-1 md:px-2 md:py-3">Actions</th>
           </tr>
         </thead>
         <tbody>
-          {activities.map((activities) => (
-            <tr className=" border-t border-gray-500 bg-pearl text-reef  hover:bg-shore hover:text-anchor">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium  whitespace-nowrap "
-              >
-                {activities.name}
+          {activities.map((activity) => (
+            <tr key={activity.id} className="border-t border-gray-500 bg-pearl text-reef hover:bg-shore hover:text-anchor">
+              <th scope="row" className="px-2 py-1 md:px-6 md:py-4 font-medium whitespace-nowrap">
+                {activity.name}
               </th>
-
-              <td className="px-6 py-4">{activities.category}</td>
-              <td className="px-6 py-4">{activities.emissions}</td>
-              <td className="px-6 py-4">{activities.unit}</td>
-              <td className=" py-4">
-                <a
-                  href="#"
-                  className="font-medium text-anchor hover:underline "
-                >
+              <td className="px-2 py-1 md:px-6 md:py-4 hidden md:table-cell">{activity.category}</td>
+              <td className="px-2 py-1 md:px-6 md:py-4 hidden md:table-cell">{activity.emissions}</td>
+              <td className="px-2 py-1 md:px-6 md:py-4 hidden md:table-cell">{activity.unit}</td>
+              <td className="px-1 py-1 md:px-2 md:py-4 flex space-x-1 md:space-x-2">
+              <Link href={`/admin/upd_activity/${activity.id}`} className="font-medium text-anchor hover:underline">
                   <Icon name="edit" size="" color="reef" />
-                </a>
-              </td>
-              <td className=" py-4">
+               </Link>
                 <a href="#" className="font-medium text-error hover:underline">
                   <Icon name="delete" size="" color="" />
                 </a>
@@ -88,14 +67,11 @@ export default function TableActivities() {
         </tbody>
       </table>
 
-      <nav
-        className="flex bg-pearl text-reef items-center flex-column flex-wrap md:flex-row justify-between pt-4  border-t border-gray-500 "
-        aria-label="Table navigation"
-      >
-        <ul className="inline-flex m-auto mb-4  -space-x-px rtl:space-x-reverse text-sm h-8 hover:bg-lightPearl">
+      <nav className="flex bg-pearl text-reef items-center flex-col md:flex-row justify-between pt-2 md:pt-4 border-t border-gray-500" aria-label="Table navigation">
+        <ul className="inline-flex m-auto mb-2 md:mb-4 -space-x-px rtl:space-x-reverse text-xs md:text-sm h-6 md:h-8">
           <li>
             <button
-              className="flex items-center justify-center px-3 h-8 ms-0 leading-tight  bg-pearl text-reef border border-anchor rounded-s-lg  font-medium  hover:bg-lightPearl hover:text-anchor"
+              className={`flex items-center justify-center px-2 md:px-3 h-6 md:h-8 ms-0 leading-tight bg-pearl border border-anchor rounded-s-lg font-medium ${!page ? 'text-gray-400' : 'text-reef hover:bg-lightPearl hover:text-anchor'}`}
               disabled={!page}
               onClick={() => setPage((prev) => prev - 1)}
             >
@@ -103,16 +79,13 @@ export default function TableActivities() {
             </button>
           </li>
           <li>
-            <a
-              href="#"
-              className="flex items-center justify-center px-7 h-8  leading-tight  bg-pearl text-reef border border-anchor   font-medium   "
-            >
+            <p className="flex items-center justify-center px-4 md:px-7 h-6 md:h-8 leading-tight bg-pearl text-reef border border-anchor font-medium">
               Page {page + 1}
-            </a>
+            </p>
           </li>
           <li>
             <button
-              className="rounded-e-lg  flex items-center justify-center px-6 h-8  leading-tight  bg-pearl text-reef border border-anchor   font-medium  hover:bg-lightPearl hover:text-anchor"
+              className="rounded-e-lg flex items-center justify-center px-4 md:px-6 h-6 md:h-8 leading-tight bg-pearl text-reef border border-anchor font-medium hover:bg-lightPearl hover:text-anchor"
               onClick={() => {
                 if (notEndPage) {
                   setPage((prev) => prev + 1);
---
+++ frontend/src/pages/admin/upd_activity/[id].tsx
@@ -0,0 +1,198 @@
+import LayoutAdmin from "@/layouts/layout-admin";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useState } from "react";
+
+import {
+  useGetActivityTypesByIdQuery,
+  useGetCategoriesQuery,
+  useGetFuelTypesQuery,
+  useGetUnitsQuery,
+  useGetVehicleDecadeQuery,
+  useGetVehicleTypesQuery,
+} from "@/graphql/generated/schema";
+
+export default function ProductDetails() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data } = useGetActivityTypesByIdQuery({
+    variables: { getActivityTypesById: parseInt(id as string) },
+    skip: typeof id === "undefined",
+  });
+
+  const activity = data?.getActivityTypesById;
+
+  const { data: d } = useGetCategoriesQuery();
+  const cats = d?.getCategories || [];
+
+  const { data: d2 } = useGetFuelTypesQuery();
+  const fuels = d2?.getFuelTypes || [];
+
+  const { data: d3 } = useGetUnitsQuery();
+  const units = d3?.getUnits || [];
+
+  const { data: d4 } = useGetVehicleDecadeQuery();
+  const decades = d4?.getVehicleDecade || [];
+
+  const { data: d5 } = useGetVehicleTypesQuery();
+  const vehiclestypes = d5?.getVehicleTypes || [];
+
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const optionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <LayoutAdmin>
+      <form className="max-w-3xl mx-auto mt-3 p-5">
+        <div>
+          <p className="pb-2 text-lg font-medium text-gray-700">
+            Modifie type activité
+          </p>
+          <h1 className="text-3xl font-bold underline text-reef">
+            {activity?.name || ""}
+          </h1>
+          <br />
+          <p className="text-sm text-gray-600 mb-4">
+            Vous avez la possibilité de modifier les caractéristiques de cette
+            type d'activité, comme la catégorie, le valeur de émission de CO
+            <sub>2</sub> et l'unité pour la mesure.
+          </p>
+          
+        </div>
+        <div className="mb-3">
+          <label
+            htmlFor="category"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Catégorie
+          </label>
+          <select
+            id="category"
+            name="category"
+            className="bg-gray-50 shadow-sm border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+            defaultValue={activity?.category}
+            value={selectedOption}
+            onChange={optionChange}
+          >
+            <option selected>{activity?.category || ""}</option>
+            {cats.map((cat) => (
+              <option value={cat} key={cat}>
+                {cat}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-3">
+          <label
+            htmlFor="emissions"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Emissions CO<sub>2</sub>
+          </label>
+          <input
+            type="text"
+            name="emissions"
+            id="emissions"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3"
+            defaultValue={activity?.emissions || ""}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label
+            htmlFor="firstName"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Unité Mesure
+          </label>
+          <select
+            id="unit"
+            name="unit"
+            className="bg-gray-50 shadow-sm border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+            defaultValue={activity?.unit || ""}
+          >
+            <option selected>{activity?.unit || ""}</option>
+            {units.map((unit) => (
+              <option value={unit} key={unit}>
+                {unit}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {(activity?.category === "Voiture" || selectedOption === "Voiture") && (
+          <div className="mb-3">
+            <label
+              htmlFor="firstName"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Type Voiture
+            </label>
+            <select
+              id="category"
+              name="category"
+              className="bg-gray-50 shadow-sm border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              defaultValue={activity?.vehicleAttributes?.vehicleType || ""}
+            >
+              {vehiclestypes.map((type) => (
+                <option value={type}>{type}</option>
+              ))}
+            </select>
+          </div>
+        )}
+
+        {(activity?.category === "Voiture" || selectedOption === "Voiture") && (
+          <div className="mb-3">
+            <label
+              htmlFor="firstName"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Carburant
+            </label>
+            <select
+              id="fuel"
+              name="fuel"
+              className="bg-gray-50 shadow-sm border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              defaultValue={activity?.vehicleAttributes?.fuelType || ""}
+            >
+              {fuels.map((fuel) => (
+                <option value={fuel}>{fuel}</option>
+              ))}
+            </select>
+          </div>
+        )}
+
+        {(activity?.category === "Voiture" || selectedOption === "Voiture") && (
+          <div className="mb-3">
+            <label
+              htmlFor="firstName"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Décennie Voiture
+            </label>
+            <select
+              id="category"
+              name="category"
+              className="bg-gray-50 shadow-sm border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              defaultValue={activity?.vehicleAttributes?.vehicleDecade || ""}
+            >
+              {decades.map((decade) => (
+                <option value={decade}>{decade}</option>
+              ))}
+            </select>
+          </div>
+        )}
+
+        <button
+          type="submit"
+          className="text-lightPearl bg-reef hover:bg-shore hover:text-anchor focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 mt-2 py-2.5 text-center"
+        >
+          Modifier
+        </button>
+      </form>
+    </LayoutAdmin>
+  );
+}
