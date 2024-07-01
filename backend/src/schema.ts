f2177b726ec0dcccf0921b5f70ee5c5fa2d6592e
096benjaminbenoit <096benjaminbenoit@gmail.com>
2024-07-01 16:53:16 +0200

    update resolvers list

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -2,9 +2,10 @@ import { buildSchema } from "type-graphql";
 import UserResolver from "./resolvers/UserResolver";
 import { authChecker } from "./auth";
 import ActivityTypeResolver from "./resolvers/ActivityTypeResolver";
+import { FollowerResolver } from "./resolvers/FollowerResolver";
 
 
 export default buildSchema({
-  resolvers: [UserResolver, ActivityTypeResolver],
+  resolvers: [UserResolver, ActivityTypeResolver, FollowerResolver],
   authChecker,
 });
