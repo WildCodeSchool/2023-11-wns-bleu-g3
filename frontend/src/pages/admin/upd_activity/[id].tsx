39b22b8afa7a71f1763de6c2e90f598eae9ee6c7
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-07-04 03:53:18 +0200

    handle submit update activity changes

--- frontend/src/pages/admin/upd_activity/[id].tsx
+++ frontend/src/pages/admin/upd_activity/[id].tsx
@@ -26,8 +26,8 @@ export default function UpdateActivity() {
   const { id } = router.query;
 
   const { data } = useGetActivityTypesByIdQuery({
-    variables: { getActivityTypesById: activId },
-    skip: typeof activId === "undefined",
+    variables: { getActivityTypesById: parseInt(id as string) },
+    skip: typeof id === "undefined",
   });
 
   const activity = data?.getActivityTypesById;
@@ -62,6 +62,8 @@ export default function UpdateActivity() {
   };
 
   // update mutation
+  // const activId = parseInt( id as string);
+
   // const activId = parseInt( id as string);
 
   const [updateActivityType] = useUpdateActivityTypeMutation();
