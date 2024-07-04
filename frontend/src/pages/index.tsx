ceb3c89d55627a55ca04e298798caf99c40ef170
alicia coquerant <alicia.coquerant@gmail.com>
2024-07-04 16:03:41 +0200

    test deploy

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -51,7 +51,7 @@ export default function Home() {
       <section className="bg-[url(https://images.unsplash.com/photo-1428908728789-d2de25dbd4e2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center bg-no-repeat">
         <div className="px-4 pt-48 sm:px-6 flex flex-col lg:justify-center h-screen items-center lg:px-8 lg:pt-10">
           <h2 className="text-2xl font-medium text-reef sm:text-4xl text-center">
-            Réduisez votre empreinte carbone, préservez notre avenir.
+            Réduisez votre empreinte carbone, préservez notre avenir. TEST CONTINUOUS DEPLOYEMENT
           </h2>
           <div className="mt-8 flex flex-col gap-4 text-center">
             <button
