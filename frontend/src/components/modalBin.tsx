6dfeaca5800c4e09e41466355a50b93a03fc61d1
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-07-05 14:09:47 +0200

    newActivtype form 1/2

--- frontend/src/components/modalBin.tsx
+++ frontend/src/components/modalBin.tsx
@@ -62,7 +62,7 @@ export default function ModalBin({ operation }: { operation: any }) {
                   <button
                     data-modal-hide="popup-modal"
                     type="button"
-                    onClick={ operation}
+                    onClick={operation}
                     className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
                   >
                     Oui, supprimer
