8d372de4734ace2657b9bd140918433c0377ea6c
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-07-10 12:05:36 +0200

    admin dashboard style 1/2

--- frontend/src/pages/admin/dashboard.tsx
+++ frontend/src/pages/admin/dashboard.tsx
@@ -1,26 +1,70 @@
 import LayoutAdmin from "@/layouts/layout-admin";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function Dashboard() {
+  const data = {
+    labels: [
+      "Positif < 2 tonnes de CO2 par an ",
+      "Acceptable: Entre 2 et 6 tonnes de CO2 par an",
+      "Négatif: > 6 tonnes de CO2 par an",
+    ],
+    datasets: [
+      {
+        data: [300, 50, 100],
+        backgroundColor: ["#3A8DA6", "#C0D6D8", "#DFE4EA"],
+        hoverBackgroundColor: ["#3A8DA6", "#C0D6D8", "#DFE4EA"],
+      },
+    ],
+  };
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        position: "right" as any,
+      },
+    },
+  };
 
-
-
-  
-
+  //example
+  const percentage = "48%";
 
   return (
     <LayoutAdmin>
       <div className="max-w-[95rem] xl m-auto h-auto mt-14 ">
         <div className="flex flex-wrap justify-between ">
-          <div className="w-[70%]    ">
+          <div className="w-[60%]">
             <div className="bg-white shadow-lg rounded-lg p-6 h-[35vh]">
-              <h2 className="text-xl font-semibold mb-4">Donations</h2>
-              <p> coming soon...</p>
+              <div className="flex">
+                <img
+                  src="../../img/bluepiggy.png"
+                  alt="petit couchon mignon"
+                  className="w-14"
+                />
+                <h2 className="text-xl font-semibold mb-4">Donations</h2>
+              </div>
+
+              <div className="w-full rounded-full bg-pearl mt-5">
+                <div
+                  className="bg-reef text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+                  style={{ width: percentage }}
+                >
+                  {percentage}
+                </div>
+              </div>
             </div>
           </div>
-          <div className="w-[27%]  ">
+          <div className="w-[38%]  ">
             <div className="bg-white shadow-lg rounded-lg p-6  h-[35vh]">
-              <h2 className="text-xl font-semibold mb-4">Stats</h2>
-              
+              <h2 className="text-lg font-semibold mb-4">
+                Consummation Moyenne Utlisateurs GreenFoot
+              </h2>
+              <div className="mt-4 h-[22vh] w-[25vw]  items-start justify-left">
+                <Doughnut data={data} options={options} />
+              </div>
             </div>
           </div>
         </div>
