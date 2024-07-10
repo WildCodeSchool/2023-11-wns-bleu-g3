4183cc017ec7d85d48e236de1cdf04a7d80b2068
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-07-10 02:39:22 +0200

    bo dashboard styling

--- frontend/src/pages/admin/dashboard.tsx
+++ frontend/src/pages/admin/dashboard.tsx
@@ -3,7 +3,30 @@ import LayoutAdmin from "@/layouts/layout-admin";
 export default function Dashboard() {
   return (
     <LayoutAdmin>
-      <h2>Je suis le dashboard</h2>
+      <div className="max-w-[95rem] xl m-auto h-auto mt-14 ">
+        <div className="flex flex-wrap justify-between ">
+          <div className="w-[70%]    ">
+            <div className="bg-white shadow-lg rounded-lg p-6 h-[35vh]">
+              <h2 className="text-xl font-semibold mb-4">Donations</h2>
+              <p> coming soon...</p>
+            </div>
+          </div>
+          <div className="w-[27%]  ">
+            <div className="bg-white shadow-lg rounded-lg p-6  h-[35vh]">
+              <h2 className="text-xl font-semibold mb-4">Stats</h2>
+              <p> coming soon...</p>
+            </div>
+          </div>
+        </div>
+        <div className="mt-6">
+          <div className="bg-white shadow-lg rounded-lg p-6 h-[35vh]">
+            <h2 className="text-xl font-semibold mb-4">Notifications</h2>
+            <p>
+            coming soon...
+            </p>
+          </div>
+        </div>
+      </div>
     </LayoutAdmin>
   );
 }
