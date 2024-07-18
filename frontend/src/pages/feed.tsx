0499da275860a1ae1e3d0123853a6fb5d1231c6c
AnaisCav <anais.cavaille93@gmail.com>
2024-07-18 17:28:49 +0200

    ajout fonction post newPost

--- frontend/src/pages/feed.tsx
+++ frontend/src/pages/feed.tsx
@@ -1,26 +1,28 @@
+import LayoutLoggedInUser from "@/layouts/layout-logged-in-user";
 import MostLikedPosts from "@/components/feed/most-liked-posts";
 import Posts from "@/components/feed/posts";
-import { useGetPostsQuery } from "@/graphql/generated/schema";
-import LayoutLoggedInUser from "@/layouts/layout-logged-in-user";
-import React from "react";
+import { useState } from "react";
+import ModalNewPost from "@/components/feed/modal-new-post";
 
 export default function Feed() {
+  const [isNewPostModalOpen, setIsNewPostModalOpen] = useState(false);
+
+  const openNewVehicleModal = () => {
+    setIsNewPostModalOpen(true);
+  };
   return (
     <LayoutLoggedInUser>
       <div className="mx-10">
         <div className="flex mb-10 justify-between">
           <h1>Bon plans</h1>
-          <div className="flex justify-end items-center gap-4">
-            <div
-              //   onClick={openNewVehicleModal}
-              className="bg-reef rounded-full object-cover h-10 w-10 text-3xl text-lightPearl flex justify-center items-center cursor-pointer"
-            >
+          <div
+            className="flex justify-end items-center gap-4 cursor-pointer"
+            onClick={openNewVehicleModal}
+          >
+            <div className="bg-reef rounded-full object-cover h-10 w-10 text-3xl text-lightPearl flex justify-center items-center ">
               +
             </div>
-            <p
-              className="text-anchor opacity-90 text-lg cursor-pointer"
-              //   onClick={openNewVehicleModal}
-            >
+            <p className="text-anchor opacity-90 text-lg ">
               Partagez un bon plan
             </p>
           </div>
@@ -29,6 +31,10 @@ export default function Feed() {
           <Posts />
           <MostLikedPosts />
         </div>
+        <ModalNewPost
+          isNewPostModalOpen={isNewPostModalOpen}
+          setIsNewPostModalOpen={setIsNewPostModalOpen}
+        />
       </div>
     </LayoutLoggedInUser>
   );
