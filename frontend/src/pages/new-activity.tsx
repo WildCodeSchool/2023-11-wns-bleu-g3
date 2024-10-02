92431c772fc3964e7fee9ec1ad4afd89c34edc66
Bertrand Robert <106388205+robertrand12@users.noreply.github.com>
2024-10-02 12:32:56 +0200

    feat: activities table (#85)

--- frontend/src/pages/new-activity.tsx
+++ frontend/src/pages/new-activity.tsx
@@ -111,8 +111,6 @@ export default function NewActivity() {
         -formJSON.ends_at.getTimezoneOffset()
       );
     } else formJSON.ends_at = null;
-    console.log("🚀 ~ handleSubmit ~ formJSON:", formJSON)
-    console.log("🚀 ~ awaitcreateActivity ~ data:", data)
 
     try {
       await newActivitySchema.validate(formJSON, { abortEarly: false });
