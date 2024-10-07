48e184913b81b55c0f5ce4c4448198c4e9ff2796
Ricardo Martinho <117286481+ricardomrcruz@users.noreply.github.com>
2024-10-07 14:08:43 +0200

    next backoffice routes secured (#100)
    * .
    
    * cleanup button

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -6,17 +6,36 @@ const JWT_PRIVATE_KEY = new TextEncoder().encode(
   process.env.JWT_PRIVATE_KEY || ""
 );
 
+// console.log({ JWT_PRIVATE_KEY: process.env.JWT_PRIVATE_KEY });
+
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
 
-  if (token) {
-    try {
-      const { payload } = await jwtVerify(token, JWT_PRIVATE_KEY);
-      if (payload.userId) return NextResponse.next();
-    } catch (e) {}
+  if (!token) return NextResponse.redirect(new URL("/", request.url));
+
+  try {
+    const { payload } = await jwtVerify(token, JWT_PRIVATE_KEY);
+
+    if (
+      request.nextUrl.pathname.startsWith("/admin") &&
+      payload.role === "admin"
+    ) {
+      return NextResponse.next();
+    }
+
+    if (token) {
+      try {
+        if (payload.userId) return NextResponse.next();
+      } catch (e) {}
+    }
+
+    return NextResponse.redirect(new URL("/", request.url));
+  } catch (error) {
+    console.error("JWT verification failed:", error);
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
-  return NextResponse.redirect(new URL("/", request.url));
+  // console.log({ token });
 }
 
 // See "Matching Paths" below to learn more
